0dc0d79948fe56ffeffdca38be2092d87dea659e
Prayaas on SP4 <prayaasa@usc.edu>
2018-08-28 10:20:26 -0700

    initial commit

---
+++ prayaasa_CSCI201L_hw1/src/prayaasa_CSCI201L_hw1/MainMenu.java
@@ -0,0 +1,42 @@
+package prayaasa_CSCI201L_hw1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+public class MainMenu
+{
+	
+	public static void main(String[] args)
+	{
+		Scanner scanObject = new Scanner(System.in);
+		String inputFilename;
+		Gson gsonObject = new Gson();
+		FileReader fr;
+		JsonObject jObject;
+		boolean validFile = false;
+		while(!validFile)
+		{
+			try
+			{
+				System.out.print("What is the name of the input file? ");
+				inputFilename =  scanObject.nextLine();
+				fr = new FileReader(inputFilename);
+				jObject = gsonObject.fromJson(fr, JsonObject.class);
+				validFile = true;
+			} catch (FileNotFoundException | JsonIOException e)
+			{
+				System.out.println("That file could not be found.");
+			} catch (JsonSyntaxException ex)
+			{
+				System.out.println("That file is not a well-formed JSON file.");
+			}
+		}
+		scanObject.close();
+	}
+
+}
